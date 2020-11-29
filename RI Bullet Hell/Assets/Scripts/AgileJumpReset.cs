f34062fa685aade834d4e9d18cb8f52c191c27f4
Nick <Nick14.clothier@gmail.com>
2020-11-29 03:05:44 +0000

    More Work On Agile Jumping Course
    Started Working On Neon Textures
    - Created New Neon Textures For Specific Blocks So Far.
    - Created Agile Course For Players to Test Agility/ Jumping
    - Created Neon Prefabs that can eaisly be duplicated
    
    To Do
    - Add Enemies throughout Course
    - Create More course that showing using cover/more movement.

---
+++ RI Bullet Hell/Assets/Scripts/AgileJumpReset.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgileJumpReset : MonoBehaviour
+{
+
+    public GameObject Player;
+    public Vector3 MyNewPos;
+
+    private void OnTriggerStay(Collider other)
+    {
+        CharacterController CC = Player.GetComponent<CharacterController>();
+        CC.enabled = false;
+        MyNewPos.Set(8, 1.08f, 3.5f);
+        Player.transform.position = MyNewPos;
+        Player.transform.rotation = Quaternion.Euler(0, 90, 0);
+        CC.enabled = true;
+
+
+    }
+}
