6c28db1b966ce74d1a89c44388f1490182a3662b
Nick <Nick14.clothier@gmail.com>
2020-12-01 11:08:03 +0000

    Created Scene Movement

---
+++ RI Bullet Hell/Assets/ResetScenes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene("Agile Course");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            SceneManager.LoadScene("Agile Course");
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
