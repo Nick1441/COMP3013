82b1415793481b5ee88428fd8fccb981b8cc7b08
Zack Hawkins <zackh019@gmail.com>
2021-01-16 21:18:19 +0000

    proc gen

---
+++ RI Bullet Hell/Assets/PerlinNoise.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerlinNoise : MonoBehaviour
+{
+
+    public static PerlinNoise instance = null;
+
+
+
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20;
+
+    public float offestX = 100;
+    public float offsetY = 100;
+
+    public int perlinGridStepSizeX;
+    public int perlinGridStepSizeY;
+
+
+    public bool visualizeObjects = false;
+    public GameObject[] Prefabs;
+    public RawImage visulizationUI;
+    private Texture2D noiseTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offestX = Random.Range(0, 99999);
+        offsetY = Random.Range(0, 99999);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GenerateTexture();
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Generate()
+    {
+        GenerateTexture();
+        if (visualizeObjects)
+        {
+            VisualizePrefabs();
+        }
+
+    }
+    void GenerateTexture()
+    {
+        noiseTexture = new Texture2D(width, height);
+
+        //generate noisemap
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                noiseTexture.SetPixel(x, y, CalculateColour(x, y));
+            }
+        }
+
+        noiseTexture.Apply();
+        visulizationUI.texture = noiseTexture;
+    }
+
+    Color CalculateColour(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offestX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+
+        return new Color(sample, sample, sample);
+    }
+
+    void VisualizePrefabs()
+    {
+        GameObject objectParent = new GameObject("ObjectParent");
+        objectParent.transform.SetParent(this.transform);
+
+        for (int x = 0; x < perlinGridStepSizeX; x++)
+        {
+            for (int y = 0; y < perlinGridStepSizeY; y++)
+            {
+                int ranSelect = Random.Range(0, Prefabs.Length);
+
+                GameObject spawn = Instantiate(Prefabs[ranSelect], new Vector3(SampleStepped(x, y),y,SampleStepped(x, y)) + transform.position, transform.rotation);
+
+                spawn.transform.SetParent(objectParent.transform);
+            }
+        }
+
+
+    }
+
+    public float SampleStepped(int x, int y)
+    {
+        int gridStepSizeX = width / perlinGridStepSizeX;
+        int gridStepSizeY = height / perlinGridStepSizeY;
+
+        float sampleFloat = noiseTexture.GetPixel((Mathf.FloorToInt(x * gridStepSizeX)), (Mathf.FloorToInt(y * gridStepSizeY))).grayscale;
+        return sampleFloat;
+    }
+
+
+
+}
---
+++ RI Bullet Hell/Assets/ProcGen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcGen : MonoBehaviour
+{
+
+    public Transform[] Prefabs;
+
+    public int NumofItems;
+
+    float treeMap = Mathf.PerlinNoise(5, 5);
+
+    public Object map;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+        for (int i = 0; i < NumofItems; i++)
+        {
+            int ranSelect = Random.Range(0, Prefabs.Length);
+
+            Vector3 spawnPoint = transform.position + new Vector3(0, 0, 8);
+
+            Quaternion spawnRotation = Quaternion.identity;
+
+            Instantiate(Prefabs[ranSelect], spawnPoint, spawnRotation);
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
