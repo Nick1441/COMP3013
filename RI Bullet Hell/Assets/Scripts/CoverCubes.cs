1019855a66ca05783a06231c91165cb3bfe3cebe
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2021-01-18 14:00:40 +0000

    fps

---
+++ RI Bullet Hell/Assets/Scripts/CoverCubes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ RequireComponent(typeof(Collider))]
+public class CoverCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider Col;
+    
+    void Start()
+    {
+        Col = GetComponent<Collider>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Cover") 
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
