9132d00725c6d937ce6d90964f2c5e357260b4b5
Nick <Nick14.clothier@gmail.com>
2021-01-19 02:00:36 +0000

    Continue Creating Game
    Created Main Menu
    - Created Font For Main Menu
    - Create Menu Navigation (whole
      Game)
    - Created Main Menu Animations/Glow
    
    Main Game
    - Created Pause Menu
    - Created Pause Menu Images/Textures
    - Changed Textures for Game Blocks
    - Created Some Test Assets For Weapons

---
+++ RI Bullet Hell/Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+    //Disable Mouse Mousemovement & Weapon Switching When Paused
+    public GameObject Player;
+    public GameObject Weapon;
+
+    //UI For Pause Menu & Bool for Paused Or Not
+    public GameObject PauseMenu;
+    public bool Paused = false;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        PauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        //Checking For Pause Menu On Main game
+        if (SceneManager.GetActiveScene().name == "Proc gen")
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (Paused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        //Issue With Mouse Disapearing On Paused? Might only be in Unity
+        if (Paused)
+        {
+            //Cursor.visible = true;
+        }
+
+    }
+    
+    void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void GameScreen()
+    {
+        SceneManager.LoadScene("Proc gen");
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+        Paused = false;
+        Time.timeScale = 1;
+        Player.GetComponent<SmoothMoveTest>().enabled = true;
+        Weapon.GetComponent<WeaponManager>().enabled = true;
+        PauseMenu.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        Paused = true;
+        Time.timeScale = 0;
+        Player.GetComponent<SmoothMoveTest>().enabled = false;
+        Weapon.GetComponent<WeaponManager>().enabled = false;
+        PauseMenu.SetActive(true);
+    }
+
+}
